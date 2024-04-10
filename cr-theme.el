e89c80643e1b1d58bb5c39fa3199c2ad3bffe6f0
roy-corentin <corentin.roy02@laposte.net>
2024-04-10 19:46:17 +0200

    [ADD] hl-todo + vi-tilde-fringe only in prog-mode

--- cr-theme.el
+++ cr-theme.el
@@ -62,13 +62,40 @@
 (use-package vi-tilde-fringe
   :ensure t
   :defer t
-  :init
-  (global-vi-tilde-fringe-mode))
+  :hook (prog-mode . vi-tilde-fringe-mode))
 
 (use-package git-gutter
   :ensure t
   :defer t
   :init
   (global-git-gutter-mode))
+
+(use-package hl-todo
+  :ensure t
+  :hook (prog-mode . hl-todo-mode)
+  :hook (yaml-mode . hl-todo-mode)
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+        hl-todo-keyword-faces
+        '(;; For reminders to change or add something at a later date.
+          ("TODO" warning bold)
+          ;; For code (or code paths) that are broken, unimplemented, or slow,
+          ;; and may become bigger problems later.
+          ("FIXME" error bold)
+          ;; For code that needs to be revisited later, either to upstream it,
+          ;; improve it, or address non-critical issues.
+          ("REVIEW" font-lock-keyword-face bold)
+          ;; For code smells where questionable practices are used
+          ;; intentionally, and/or is likely to break in a future update.
+          ("HACK" font-lock-constant-face bold)
+          ;; For sections of code that just gotta go, and will be gone soon.
+          ;; Specifically, this means the code is deprecated, not necessarily
+          ;; the feature it enables.
+          ("DEPRECATED" font-lock-doc-face bold)
+          ;; Extra keywords commonly found in the wild, whose meaning may vary
+          ;; from project to project.
+          ("NOTE" success bold)
+          ("BUG" error bold)
+          ("XXX" font-lock-constant-face bold))))
 
 (provide 'cr-theme)
