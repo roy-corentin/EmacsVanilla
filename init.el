f641d00f0101d3024e8468ee4226aaa0e5979d9a
roy-corentin <corentin.roy02@laposte.net>
2024-04-10 17:43:11 +0200

    [UP] improve treemacs conf + add treemacs-nerd-icons/magit

--- init.el
+++ init.el
@@ -25,6 +25,10 @@
 (use-package treemacs
   :ensure t
   :defer t
+  :init
+  (setq treemacs-follow-after-init t
+        treemacs-is-never-other-window t
+        treemacs-sorting 'alphabetic-case-insensitive-asc)
   :config
   ;; Add ignored files and file extensions
   (setq treemacs-file-ignore-extensions '("o" "gcna" "gcdo" "vscode" "idea")
@@ -35,13 +39,22 @@
         (cl-loop for glob in treemacs-file-ignore-globs
                  thereis (file-name-match-glob glob full-path))))
   (add-to-list 'treemacs-ignored-file-predicates #'my-treemacs-ignore-filter)
-
-  ;; Enable follow mode
-  (treemacs-follow-mode t)
-  (lsp-treemacs-sync-mode t)
-
   ;; Set treemacs theme
   (setq doom-themes-treemacs-theme "doom-colors"))
+
+(use-package treemacs-nerd-icons
+  :ensure t
+  :after treemacs
+  :config (treemacs-load-theme "nerd-icons"))
+
+
+(use-package treemacs-evil
+  :ensure t
+  :after treemacs)
+
+(use-package treemacs-magit
+  :ensure t
+  :after treemacs magit)
 
 (use-package which-key
   :ensure t
