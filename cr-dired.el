9c67d9a9889d1a7119f3548095e3523a09bef298
roy-corentin <corentin.roy02@laposte.net>
2024-04-11 22:27:52 +0200

    [ADD] dired config file

---
+++ cr-dired.el
@@ -0,0 +1,24 @@
+;;; cr-dired.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Corentin Roy
+;;
+;; Author: Corentin Roy <corentin.roy02@laposte.net>
+;; Maintainer: Corentin Roy <corentin.roy02@laposte.net>
+;; Created: avril 11, 2024
+
+(with-eval-after-load 'dired
+  (evil-define-key 'normal dired-mode-map (kbd "h") 'dired-up-directory)
+  (evil-define-key 'normal dired-mode-map (kbd "l") 'dired-find-file)) ; use dired-open-file or dired-find-file instead if not using dired-open package
+
+(use-package nerd-icons-dired
+  :ensure t
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package diredfl
+  :ensure t
+  :init
+  (diredfl-global-mode))
+
+(provide 'cr-dired)
+;;; cr-dired.el ends here
