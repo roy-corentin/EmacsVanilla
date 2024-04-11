de7a6ffb8cfe390a5b59d37f4368867d501daf3a
roy-corentin <corentin.roy02@laposte.net>
2024-04-11 21:30:34 +0200

    [UP] org-level-1 size face

--- cr-org.el
+++ cr-org.el
@@ -15,7 +15,7 @@
 
 (defun cr/org-font-setup ()
   ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.4)
+  (dolist (face '((org-level-1 . 1.3)
                   (org-level-2 . 1.2)
                   (org-level-3 . 1.1)
                   (org-level-4 . 1.1)
