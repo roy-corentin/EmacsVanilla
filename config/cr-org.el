ae89ef2261484d736623b3cbe2175492414bcb39
roy-corentin <corentin.roy02@laposte.net>
2024-04-13 22:49:38 +0200

    [ADD] org-roam-ui package

--- config/cr-org.el
+++ config/cr-org.el
@@ -265,5 +265,22 @@
                          "#+title: ${title}\n#+filetags: :DATASTRUCTURE:\n")
       :unnarrowed t))))
 
+(use-package websocket
+  :ensure t
+  :after org-roam)
+
+(use-package org-roam-ui
+  :ensure t
+  :after org-roam ;; or :after org
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start nil))
+
 (provide 'cr-org)
 ;;; cr-org.el ends here
