7765b3c9de65b21af468e09c89ee4e4bd75f04a2
roy-corentin <corentin.roy02@laposte.net>
2024-04-14 01:36:53 +0200

    [UP] move doom methods in another file

--- config/cr-keybindings.el
+++ config/cr-keybindings.el
@@ -6,32 +6,7 @@
 ;; Maintainer: Corentin Roy <corentin.roy02@laposte.net>
 ;; Created: avril 08, 2024
 
-;; From Doom
-(defun +evil/window-split-and-follow ()
-  "Split current window horizontally, then focus new window.
-If `evil-split-window-below' is non-nil, the new window isn't focused."
-  (interactive)
-  (let ((evil-split-window-below (not evil-split-window-below)))
-    (call-interactively #'evil-window-split)))
-
-;; From Doom
-(defun +evil/window-vsplit-and-follow ()
-  "Split current window vertically, then focus new window.
-If `evil-vsplit-window-right' is non-nil, the new window isn't focused."
-  (interactive)
-  (let ((evil-vsplit-window-right (not evil-vsplit-window-right)))
-    (call-interactively #'evil-window-vsplit)))
-
-;; From Doom
-(defun +vertico/embark-preview ()
-  "Previews candidate in vertico buffer, unless it's a consult command"
-  (interactive)
-  (unless (bound-and-true-p consult--preview-function)
-    (if (fboundp 'embark-dwim)
-        (save-selected-window
-          (let (embark-quit-after-action)
-            (embark-dwim)))
-      (user-error "Embark not installed, aborting..."))))
+(require 'doom-methods)
 
 
 (defun cr/find-file-in-dir(dir)
---
+++ config/doom-methods.el
@@ -0,0 +1,37 @@
+;;; -*- lexical-binding: t -*-
+
+;; To prevent "Defining as dynamic an already lexical var" from +vertico/embark-preview
+;;;###autoload
+(defvar embark-quit-after-action)
+
+;; From Doom
+;;;###autoload
+(defun +evil/window-split-and-follow ()
+  "Split current window horizontally, then focus new window.
+If `evil-split-window-below' is non-nil, the new window isn't focused."
+  (interactive)
+  (let ((evil-split-window-below (not evil-split-window-below)))
+    (call-interactively #'evil-window-split)))
+
+;; From Doom
+;;;###autoload
+(defun +evil/window-vsplit-and-follow ()
+  "Split current window vertically, then focus new window.
+If `evil-vsplit-window-right' is non-nil, the new window isn't focused."
+  (interactive)
+  (let ((evil-vsplit-window-right (not evil-vsplit-window-right)))
+    (call-interactively #'evil-window-vsplit)))
+
+;; From Doom
+;;;###autoload
+(defun +vertico/embark-preview ()
+  "Previews candidate in vertico buffer, unless it's a consult command"
+  (interactive)
+  (unless (bound-and-true-p consult--preview-function)
+    (if (fboundp 'embark-dwim)
+        (save-selected-window
+          (let (embark-quit-after-action)
+            (embark-dwim)))
+      (user-error "Embark not installed, aborting..."))))
+
+(provide 'doom-methods)
