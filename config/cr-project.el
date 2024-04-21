e06d1e894b744945536041e7f4310f457f57affd
roy-corentin <corentin.roy02@laposte.net>
2024-04-21 11:49:07 +0200

    [UP] only one extra root marker (simpler)

--- config/cr-project.el
+++ config/cr-project.el
@@ -12,13 +12,8 @@
 
 (use-package project
   :custom
-  (project-known-project-roots)
-  :config
-  (setq
-   project-vc-ignores
-   '("target/" "bin/" "obj/" "node_modules/" "_build/")
-   project-vc-extra-root-markers
-   '(".project"))
-  )
+  (project-vc-ignores '("target/" "bin/" "obj/" "node_modules/" "_build/"))
+  (project-vc-extra-root-markers '(".project"))
+  (project-known-project-roots))
 
 (provide 'cr-project)
