5482340997c8b31a70149f67f305738cac0a6d2e
roy-corentin <corentin.roy02@laposte.net>
2024-05-14 23:21:42 +0200

    [ADD] consult-org-roam

--- config/cr-org.el
+++ config/cr-org.el
@@ -342,6 +342,26 @@
                 (svg-tag-make tag :end -1 :inverse t :crop-left t :margin 0 :face 'org-date))))
           )))
 
+(use-package consult-org-roam
+  :ensure t
+  :after org-roam
+  :init
+  (require 'consult-org-roam)
+  ;; Activate the minor mode
+  (consult-org-roam-mode 1)
+  :custom
+  ;; Use `ripgrep' for searching with `consult-org-roam-search'
+  (consult-org-roam-grep-func #'consult-ripgrep)
+  ;; Configure a custom narrow key for `consult-buffer'
+  (consult-org-roam-buffer-narrow-key ?r)
+  ;; Display org-roam buffers right after non-org-roam buffers
+  ;; in consult-buffer (and not down at the bottom)
+  (consult-org-roam-buffer-after-buffers t)
+  :config
+  ;; Eventually suppress previewing for certain functions
+  (consult-customize
+   consult-org-roam-forward-links
+   :preview-key "M-."))
 
 (provide 'cr-org)
 ;;; cr-org.el ends here
