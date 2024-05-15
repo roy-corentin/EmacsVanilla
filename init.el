a3079725f38cbc6926eccd5d8a747b4fa181db86
roy-corentin <corentin.roy02@laposte.net>
2024-05-15 22:24:03 +0200

    [DEL] custom-set-variables from init.el

--- init.el
+++ init.el
@@ -113,24 +113,3 @@
 ;; Key set at the end to avoid conflicts with iedit
 (global-set-key (kbd "C-,") 'previous-buffer)
 (global-set-key (kbd "C-;") 'next-buffer)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" default))
- '(package-selected-packages
-   '(svg-tag-mode yasnippet-capf ruby-end load-env-vars crystal-mode solaire-mode verb verb-mode zig-mode emacs-surround which-key wgrep vundo vterm vi-tilde-fringe vertico undo-fu-session undo-fu treesit-auto treemacs-nerd-icons treemacs-magit treemacs-evil toc-org snippets smartparens restclient rainbow-delimiters pdf-tools org-roam-ui org-bullets orderless olivetti nerd-icons-dired nerd-icons-corfu nerd-icons-completion mixed-pitch meow markdown-mode marginalia magit-todos keycast git-gutter general evil-surround evil-org evil-multiedit evil-mc evil-lion evil-goggles evil-escape evil-collection evil-anzu embark-consult elixir-ts-mode eglot-booster eglot editorconfig drag-stuff doom-themes doom-modeline docker diredfl dashboard csv-mode csv corfu copilot casual cape auto-yasnippet apheleia)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-goggles-change-face ((t (:inherit diff-removed))))
- '(evil-goggles-delete-face ((t (:inherit diff-removed))))
- '(evil-goggles-paste-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
- '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
- '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
