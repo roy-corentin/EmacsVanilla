43e66877aabf83758e65a7d00bdcf58b5a9041e9
roy-corentin <corentin.roy02@laposte.net>
2024-06-03 19:42:47 +0200

    [ADD] doom-snippets + only add custom snippet directory to yas list

--- config/cr-yasnippet.el
+++ config/cr-yasnippet.el
@@ -6,14 +6,17 @@
 ;; Maintainer: Corentin Roy <corentin.roy02@laposte.net>
 ;; Created: avril 13, 2024
 
+(use-package doom-snippets
+  :ensure (:protocol https :inherit t :depth 1 :fetcher github :repo "doomemacs/snippets" :files (:defaults))
+  :defer t)
+
 (use-package yasnippet
   :ensure t
   :defer t
   :init
   (defvar yas-verbosity 3)
   :config
-  (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "elpa/snippets/"))
-  (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "elpaca/repos/doom-snippets/"))
+  (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "elpaca/repos/snippets/"))
   (yas-global-mode 1))
 
 (use-package auto-yasnippet 
