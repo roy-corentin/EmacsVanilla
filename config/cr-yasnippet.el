739739446af1c9be75bfb4313f68e4cf37ee28f8
roy-corentin <corentin.roy@epitech.eu>
2024-06-05 23:28:51 +0200

    [UP] yas setup

--- config/cr-yasnippet.el
+++ config/cr-yasnippet.el
@@ -21,18 +21,9 @@
 
 (use-package auto-yasnippet 
   :ensure t
-  :defer t
+  :after yasnippet
   :config
-  (setq aya-persist-snippets-dir +snippets-dir)
-  (defadvice! +snippets--inhibit-yas-global-mode-a (fn &rest args)
-              "auto-yasnippet enables `yas-global-mode'. This is obnoxious for folks like
-us who use yas-minor-mode and enable yasnippet more selectively. This advice
-swaps `yas-global-mode' with `yas-minor-mode'."
-              :around '(aya-expand aya-open-line)
-              (letf! ((#'yas-global-mode #'yas-minor-mode)
-                      (yas-global-mode yas-minor-mode))
-                     (apply fn args))))
-
+  (setq aya-persist-snippets-dir (concat user-emacs-directory "snippets/")))
 
 (use-package autoinsert
   :ensure nil
