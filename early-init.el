012ea285481695032cb97870d0772445c687becd
roy-corentin <corentin.roy@epitech.eu>
2024-06-28 11:51:26 +0200

    [LINT] remove empty lines

--- early-init.el
+++ early-init.el
@@ -89,11 +89,9 @@
   (display-line-numbers-current-absolute t)
   (indent-tabs-mode nil)
   (tab-always-indent 'complete)
-
   ;; Emacs 30 and newer: Disable Ispell completion function. As an alternative,
   ;; try `cape-dict'.
   (setq text-mode-ispell-word-completion nil)
-
   ;; Emacs 28 and newer: Hide commands in M-x which do not apply to the current
   ;; mode.  Corfu commands are hidden, since they are not used via M-x. This
   ;; setting is useful beyond Corfu.
