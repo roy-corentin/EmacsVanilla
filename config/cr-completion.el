2e7a770680076822ab69017a7c65df37eb620ce0
roy-corentin <corentin.roy@epitech.eu>
2024-07-26 18:58:07 +0200

    [UP] orderless config (follow README recommendations)

--- config/cr-completion.el
+++ config/cr-completion.el
@@ -111,13 +111,13 @@
 
 (use-package orderless
   :ensure t
-  :init
+  :custom
   ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package consult
   :ensure t
