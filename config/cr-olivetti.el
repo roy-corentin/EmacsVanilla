b15fa9da15c353b264f5b3db5f097ca8dc20dfe7
roy-corentin <corentin.roy@epitech.eu>
2024-08-21 17:43:18 +0200

    [UP] make custom olivetti method interactive

--- config/cr-olivetti.el
+++ config/cr-olivetti.el
@@ -8,6 +8,7 @@
 
 (defun cr/olivetti-on-single-prog-window (&rest args)
   "Toggle Olivetti mode based on the window configuration"
+  (interactive)
   (when (or (derived-mode-p 'prog-mode) (derived-mode-p 'dired-mode) (derived-mode-p 'conf-mode))
     (olivetti-mode 0)
     (if (>= (window-width) (floor (frame-width) 2))
