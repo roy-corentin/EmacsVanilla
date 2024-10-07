815923bf873b35a67083fb50adfbb72a97c68306
roy-corentin <corentin.roy@epitech.eu>
2024-10-07 18:55:28 +0200

    [UP] improve customization compile-command for modes

--- config/cr-compile.el
+++ config/cr-compile.el
@@ -6,18 +6,29 @@
 ;; Maintainer: Corentin Roy <corentin.roy02@laposte.net>
 ;; Created: avril 10, 2024
 
+(defun run-with-typescript ()
+  "Set the default compil-command to run the current typescript project"
+  (setq-local compile-command
+              (cond ((file-exists-p (concat (project-root(project-current)) "pnpm-lock.yaml")) "pnpm tsc")
+                    ((file-exists-p (concat (project-root(project-current)) "package-lock.json")) "npm run tsc")
+                    ((file-exists-p (concat (project-root(project-current)) "bun.lockb")) "bun tsc"))))
+
+(defun run-with-python ()
+  "Set the default compil-command to run the current file with python"
+  (setq-local compile-command
+              (concat "python3 "
+                      (when buffer-file-name
+                        shell-quote-argument buffer-file-name))))
+
+
 (use-package compile
   :ensure nil
   :config
   (push 'bun-test compilation-error-regexp-alist)
   (push '(bun-test "\sat\s\\([a-zA-Z0-9/\\._-]+\\):\\([0-9]+\\):\\([0-9]+\\)" 1 2 3)
         compilation-error-regexp-alist-alist)
-  (add-hook 'typescript-ts-mode
-            (lambda ()
-              (set (make-local-variable 'compile-command)
-                   (cond ((file-exists-p (concat (project-root(project-current)) "pnpm-lock.yaml")) "pnpm tsc")
-                         ((file-exists-p (concat (project-root(project-current)) "package-lock.json")) "npm tsc")
-                         ((file-exists-p (concat (project-root(project-current)) "bun.lockb")) "bun tsc"))))))
+  (add-hook 'typescript-ts-mode-hook 'run-with-typescript)
+  (add-hook 'python-ts-mode-hook 'run-with-python))
 
 (provide 'cr-compile)
 ;;; cr-compile.el ends here
