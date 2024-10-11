613639c66477b859af9b2b83c1f800ea48f0562f
roy-corentin <corentin.roy@epitech.eu>
2024-10-11 21:40:57 +0200

    [UP] refacto setup compile to set compile-command at project switch based on files in it

--- config/cr-compile.el
+++ config/cr-compile.el
@@ -6,20 +6,17 @@
 ;; Maintainer: Corentin Roy <corentin.roy02@laposte.net>
 ;; Created: avril 10, 2024
 
-(defun run-with-typescript ()
-  "Set the default compil-command to run the current typescript project"
+(defun cr/set-compile-command (&rest args)
+  "Set the default compil-command to run the current project"
   (setq-local compile-command
-              (cond ((file-exists-p (concat (project-root(project-current)) "pnpm-lock.yaml")) "pnpm tsc")
+              (cond ((file-exists-p (concat (project-root(project-current)) "Gemfile")) "ruby ")
+                    ((file-exists-p (concat (project-root(project-current)) "requirements.txt"))
+                     (concat "python3 "
+                             (when buffer-file-name
+                               (shell-quote-argument buffer-file-name))))
+                    ((file-exists-p (concat (project-root(project-current)) "pnpm-lock.yaml")) "pnpm tsc")
                     ((file-exists-p (concat (project-root(project-current)) "package-lock.json")) "npm run tsc")
                     ((file-exists-p (concat (project-root(project-current)) "bun.lockb")) "bun tsc"))))
-
-(defun run-with-python ()
-  "Set the default compil-command to run the current file with python"
-  (setq-local compile-command
-              (concat "python3 "
-                      (when buffer-file-name
-                        (shell-quote-argument buffer-file-name)))))
-
 
 (use-package compile
   :ensure nil
@@ -27,11 +24,10 @@
   (compilation-scroll-output t)
   :config
   (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
+  (advice-add #'project-switch-project :after #'cr/set-compile-command)
   (push 'bun-test compilation-error-regexp-alist)
   (push '(bun-test "\sat\s\\([a-zA-Z0-9/\\._-]+\\):\\([0-9]+\\):\\([0-9]+\\)" 1 2 3)
-        compilation-error-regexp-alist-alist)
-  (add-hook 'typescript-ts-mode-hook 'run-with-typescript)
-  (add-hook 'python-ts-mode-hook 'run-with-python))
+        compilation-error-regexp-alist-alist))
 
 (provide 'cr-compile)
 ;;; cr-compile.el ends here
