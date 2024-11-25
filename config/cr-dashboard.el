918a92a034d7218c08d1287f700facc809328238
roy-corentin <corentin.roy@epitech.eu>
2024-11-25 17:13:17 +0100

    [UP] use :custom instead of setq list

--- config/cr-dashboard.el
+++ config/cr-dashboard.el
@@ -17,17 +17,17 @@
 (use-package dashboard
   :ensure t
   :requires nerd-icons
-  :init
-  (setq dashboard-display-icons-p t)     ; display icons on both GUI and terminal
-  (setq dashboard-icon-type 'nerd-icons) ; use `nerd-icons' package
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-center-content t)
-  (setq dashboard-items '((recents   . 5)
-                          (projects  . 5)
-                          (agenda    . 5)
-                          (bookmarks . 5)
-                          (registers . 5)))
+  :custom
+  (dashboard-display-icons-p t)     ; display icons on both GUI and terminal
+  (dashboard-icon-type 'nerd-icons) ; use `nerd-icons' package
+  (dashboard-set-heading-icons t)
+  (dashboard-set-file-icons t)
+  (dashboard-center-content t)
+  (dashboard-items '((recents   . 5)
+                     (projects  . 5)
+                     (agenda    . 5)
+                     (bookmarks . 5)
+                     (registers . 5)))
   :config
   (my-dashboard-setup-startup-hook))
 
