48c477de149f7067a81416cbfd2a3e639e4244a4
roy-corentin <corentin.roy@epitech.eu>
2024-11-27 22:41:18 +0100

    [FIX] yasnippet setup

--- config/cr-yasnippet.el
+++ config/cr-yasnippet.el
@@ -6,15 +6,17 @@
 ;; Maintainer: Corentin Roy <corentin.roy02@laposte.net>
 ;; Created: avril 13, 2024
 
-(use-package doom-snippets
-  :ensure (:protocol https :inherit t :depth 1 :fetcher github :repo "doomemacs/snippets" :files (:defaults)))
-
 (use-package yasnippet
   :ensure t
   :custom
-  (yas-snippet-dirs '((concat user-emacs-directory "elpaca/repos/snippets/"))(concat user-emacs-directory "snippets/"))
+  (yas-snippet-dirs '("~/.config/emacs/elpaca/repos/snippets/"
+                      "~/.config/emacs/snippets/"))
   (yas-global-mode 1)
   (yas-verbosity 3))
+
+(use-package doom-snippets
+  :ensure (:protocol https :inherit t :depth 1 :fetcher github :repo "doomemacs/snippets" :files (:defaults))
+  :after yasnippet)
 
 (use-package auto-yasnippet
   :ensure t
