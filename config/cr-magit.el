0b1e1b40c5028819b9f482aead611d940d8b96d0
roy-corentin <corentin.roy@epitech.eu>
2025-01-20 11:58:25 +0100

    [UP] comment forge package

--- config/cr-magit.el
+++ config/cr-magit.el
@@ -60,10 +60,10 @@
   :config
   (magit-file-icons-mode 1))
 
-(use-package forge
-  :ensure (:protocol https :inherit t :depth 1 :fetcher github :repo "magit/forge" :files (:defaults) :branch "main")
-  :after magit
-  :custom
-  (forge-add-default-bindings nil))
+;; (use-package forge
+;;   :ensure (:protocol https :inherit t :depth 1 :fetcher github :repo "magit/forge" :files (:defaults) :branch "main")
+;;   :after magit
+;;   :custom
+;;   (forge-add-default-bindings nil))
 
 (provide 'cr-magit)
