ca9897d5fd1a18cf69790eea76c1075b58763353
roy-corentin <corentin.roy@epitech.eu>
2025-01-27 10:14:14 +0100

    [UP] custom flymake indicator set to hook prog-mode instead of olivetti-mode

--- config/cr-flymake.el
+++ config/cr-flymake.el
@@ -12,7 +12,7 @@
   :preface
   (defun cr/set-flymake-indicator-type ()
     (setq-local flymake-indicator-type (if olivetti-mode 'fringes 'margins)))
-  :hook (olivetti-mode . cr/set-flymake-indicator-type)
+  :hook (prog-mode . cr/set-flymake-indicator-type)
   :custom
   (flymake-show-diagnostics-at-end-of-line 'short)
   (flymake-indicator-type 'margins))
