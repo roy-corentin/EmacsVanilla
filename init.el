08e503c8f609e7e0cad65eb318aaeb7ce1ff211f
roy-corentin <corentin.roy@epitech.eu>
2025-02-14 19:48:10 +0100

    [UP] customize some tramp variable to speed up connections

--- init.el
+++ init.el
@@ -123,6 +123,19 @@
 (use-package casual
   :ensure t)
 
+(use-package tramp
+  :config
+  ;; Enable full-featured Dirvish over TRAMP on certain connections
+  ;; https://www.gnu.org/software/tramp/#Improving-performance-of-asynchronous-remote-processes-1.
+  (add-to-list 'tramp-connection-properties
+               (list (regexp-quote "/ssh:YOUR_HOSTNAME:")
+                     "direct-async-process" t))
+  :custom
+  ;; Tips to speed up connections
+  (tramp-verbose 0)
+  (tramp-chunksize 2000)
+  (tramp-use-ssh-controlmaster-options nil))
+
 (require 'cr-org)
 (require 'cr-term)
 (require 'cr-buffer)
@@ -146,3 +159,32 @@
 (require 'cr-flymake)
 (require 'cr-kubernetes)
 (require 'cr-global-keybindings)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6"
+     "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd"
+     "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" default))
+ '(package-selected-packages
+   '(ace-window cape corfu-terminal dape diff-hl dired-preview eglot embark-consult
+                evil-anzu evil-escape evil-goggles evil-indent-plus
+                evil-surround gcmh gnuplot keycast marginalia markdown-mode meow
+                orderless org-contacts pdf-tools rainbow-delimiters rainbow-mode
+                svg-tag-mode toc-org undo-fu undo-fu-session vertico-posframe
+                vundo websocket wgrep ws-butler yasnippet zig-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
