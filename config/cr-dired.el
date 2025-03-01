f007d0196b8ec826e352af8d5f495741992efebd
roy-corentin <corentin.roy@epitech.eu>
2025-03-01 18:44:53 +0100

    [UP] update dirvish config

--- config/cr-dired.el
+++ config/cr-dired.el
@@ -39,6 +39,7 @@
   (dirvish-use-mode-line nil)
   (dirvish-subtree-always-show-state t)
   (dirvish-attributes '(file-size collapse nerd-icons vc-state subtree-state))
+  (dirvish-side-attributes '(file-size  collapse nerd-icons vc-state))
   (delete-by-moving-to-trash t)
   (dirvish-path-separators (list
                             (format "  %s " (nerd-icons-codicon "nf-cod-home"))
@@ -52,9 +53,11 @@
     :require ("eza") ; tell Dirvish to check if we have the executable
     (when (file-directory-p file) ; we only interest in directories here
       `(shell . ("eza" "-laS" "--icons=always" "--color=always"
-                 "--group-directories-first", file))))
-  (add-to-list 'dirvish-preview-dispatchers 'eza)
+                 "--group-directories-first" ,file))))
+  (setq dirvish-preview-dispatchers
+        (cl-substitute 'eza 'dired dirvish-preview-dispatchers))
   (dirvish-side-follow-mode)
+  :init
   (dirvish-override-dired-mode))
 
 (use-package nerd-icons-dired
