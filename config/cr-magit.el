443b4c90d9355dd0bfadf24be000fd6309116d6e
roy-corentin <corentin.roy@epitech.eu>
2026-01-02 08:01:30 +0100

    [UP] magit setup

--- config/cr-magit.el
+++ config/cr-magit.el
@@ -26,14 +26,13 @@
   :bind ("C-x g" . magit-status)
   :custom
   (magit-blame-echo-style 'headings)
-  (magit-auto-revert-mode nil)
   (magit-refresh-status-buffer nil)
   (transient-default-level 5)
   (magit-diff-refine-hunk t) ; show granular diffs in selected hunk
   ;; Don't autosave repo buffers. This is too magical, and saving can
   ;; trigger a bunch of unwanted side-effects, like save hooks and
   ;; formatters. Trust the user to know what they're doing.
-  (magit-save-repository-buffers nil)
+  (magit-save-repository-buffers t)
   ;; Don't display parent/related refs in commit buffers; they are rarely
   ;; helpful and only add to runtime costs.
   (magit-revision-insert-related-refs nil)
