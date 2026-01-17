3ff77a429f3f628fb7f8bf762e61a48837a0ceb5
roy-corentin <corentin.roy@epitech.eu>
2026-01-17 05:15:41 +0100

    [UP] increase flymake-no-changes-timeout + make it start in evil normal state entry

--- config/cr-flymake.el
+++ config/cr-flymake.el
@@ -14,9 +14,10 @@
   :ensure nil
   :hook prog-mode
   :hook (tty-setup . (lambda () (setq flymake-show-diagnostics-at-end-of-line 'fancy)))
+  :hook (evil-normal-state-entry . (lambda () (when (fboundp 'flymake-start) (flymake-start))))
   :custom
   (flymake-show-diagnostics-at-end-of-line 'fancy)
-  (flymake-no-changes-timeout 0.25))
+  (flymake-no-changes-timeout 30))
 
 (use-package flyover
   :ensure t
