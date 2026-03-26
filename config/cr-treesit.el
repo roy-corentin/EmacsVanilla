a813ec5213e3a50417b3c62a41d4c27269bb9d1b
roy-corentin <corentin.roy@epitech.eu>
2026-03-26 04:14:55 +0100

    [UP] decrease treesit-font-lock-level to 3 and update zig grammar url

--- config/cr-treesit.el
+++ config/cr-treesit.el
@@ -12,15 +12,14 @@
 
 (use-package treesit
   :ensure nil
-  :defer t
   :custom
-  (treesit-font-lock-level 4)
+  (treesit-font-lock-level 3)
   (treesit-enabled-modes t)
   :config
   (add-to-list 'treesit-language-source-alist '(go "https://github.com/tree-sitter/tree-sitter-go"))
   (add-to-list 'treesit-language-source-alist '(css "https://github.com/tree-sitter/tree-sitter-css"))
   (add-to-list 'treesit-language-source-alist '(python "https://github.com/tree-sitter/tree-sitter-python"))
-  (add-to-list 'treesit-language-source-alist '(zig "https://github.com/tree-sitter-grammars/tree-sitter-zig"))
+  (add-to-list 'treesit-language-source-alist '(zig "https://github.com/tree-sitter-grammars/tree-sitter-zig" :commit "6479aa13f32f701c383083d8b28360ebd682fb7d"))
   (add-to-list 'treesit-language-source-alist '(rust "https://github.com/tree-sitter/tree-sitter-rust"))
   (add-to-list 'treesit-language-source-alist '(toml "https://github.com/tree-sitter/tree-sitter-toml"))
   (add-to-list 'treesit-language-source-alist '(js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")))
