6459ebb4de25e5e53c08454260acf6468d03f5e4
roy-corentin <corentin.roy@epitech.eu>
2026-05-16 16:11:37 +0200

    refactor(config/cr-buffer): standardize display buffer settings
    - Refactored display buffer configurations to use a consistent alist
     syntax for all buffer targets.
    - Changed tab group handling to use alist syntax with display functions and parameters properly grouped.

--- config/cr-buffer.el
+++ config/cr-buffer.el
@@ -25,47 +25,48 @@
   (display-buffer-alist
    '(
      ((or . ((derived-mode . help-mode)
-             "\\*\\(eldoc.*\\|vterm-popup-.*\\|helpful.*\\)\\*"))
+             "\\*\\(eldoc.*\\|vterm-popup-.*\\|helpful.*\\)\\*")) .
+             ;; List display function
+             ((display-buffer-in-side-window) .
+              ;; Parameter
+              ((side . bottom)
+               (dedicated . t)
+               (body-function . select-window)
+               (slot . 0)
+               (window-parameters . ((mode-line-format . none))))))
+     ("\\*Embark Actions\\*" .
       ;; List display function
-      (display-buffer-in-side-window)
-      ;; Parameter
-      (side . bottom)
-      (dedicated . t)
-      (body-function . select-window)
-      (slot . 0)
-      (window-parameters . ((mode-line-format . none))))
-     ("\\*Embark Actions\\*"
+      ((display-buffer-in-side-window) .
+       ;; Parameter
+       ((side . bottom)
+        (dedicated t)
+        (window-height . fit-window-to-buffer))))
+     ("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*" .
+      (nil .
+           ((window-parameters (mode-line-format . none)))))
+     (".*todo.org" .
       ;; List display function
-      (display-buffer-in-side-window)
-      ;; Parameter
-      (side . bottom)
-      (dedicated t)
-      (window-height . fit-window-to-buffer))
-     ("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-      nil
-      (window-parameters (mode-line-format . none)))
-     (".*todo.org"
-      ;; List display function
-      (display-buffer-in-tab)
-      ;; Parameter
-      (tab-name . "todo"))
-     ("^\\*elfeed-entry-"
-      (display-buffer-in-tab)
-      (dedicated . t)
-      (tab-name . (lambda (buffer alist)
-                    (with-current-buffer buffer
-                      (elfeed-feed-title (elfeed-entry-feed elfeed-show-entry)))))
-      (tab-group . "Elfeed"))
-     ("\\*elfeed-search\\*"
-      (display-buffer-in-tab)
-      (dedicated . t)
-      (tab-name . "Entries")
-      (tab-group . "Elfeed"))
-     (cr-buffer-has-project-p
-      ;; List display function
-      (display-buffer-in-tab)
-      ;; Parameter
-      (tab-name . cr-tab-tab-name)))))
+      ((display-buffer-in-tab) .
+       ;; Parameter
+       ((tab-name . "todo"))))
+     ("^\\*elfeed-entry-" .
+      ((display-buffer-in-tab) .
+       ((dedicated . t)
+        (tab-name . (lambda (buffer alist)
+                      (with-current-buffer buffer
+                        (elfeed-feed-title (elfeed-entry-feed elfeed-show-entry)))))
+        (tab-group . "Elfeed"))))
+     ("\\*elfeed-.*" .
+      ((display-buffer-in-tab buffer alist) .
+       ((tab-name . "Entries")
+        (dedicated . t)
+        (tab-group . "Elfeed"))))
+     (cr-buffer-has-project-p .
+                              ;; List display function
+                              ((display-buffer-in-tab) .
+                               ;; Parameter
+                               ((tab-name . cr-tab-tab-name))))
+     )))
 
 (use-package nerd-icons-ibuffer
   :ensure t
