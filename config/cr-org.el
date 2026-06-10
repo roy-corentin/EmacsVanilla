5e01e303741fe063c9803e6636f64729895a914a
roy-corentin <corentin.roy@epitech.eu>
2026-06-10 16:21:15 +0200

    refactor(org): defer org-contacts and not org-roam

--- config/cr-org.el
+++ config/cr-org.el
@@ -202,12 +202,12 @@
 
 (use-package org-contacts
   :ensure t
+  :defer t
   :custom
   (org-contacts-files '("~/org/contacts.org")))
 
 (use-package org-roam
   :ensure t
-  :defer t
   :custom
   (org-roam-directory  "~/Dropbox/RoamNotes/")
   (org-roam-index-file "~/Dropbox/RoamNotes/index.org")
